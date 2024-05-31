4f9509384d0c05525d7ac564f0d25d5e4a83077c
jay kukadiya <jaykukadiya45@gmail.com>
2024-05-31 10:10:35 +0530

    static-project

---
+++ src/components/home/faqsRow/Faq.jsx
@@ -0,0 +1,35 @@
+import styles from "../../../styles/home/faqsRow.module.css";
+
+function Faq(props) {
+  const { question, answer, number, index, selectedFaq, setSelectedFaq } =
+    props;
+
+  function handleClick() {
+    setSelectedFaq(index);
+  }
+
+  console.log("selectedFaq", selectedFaq);
+
+  return (
+    <div className={styles.faqContainer}>
+      <div className={styles.faqQueContainer}>
+        <p>
+          <span>{number}</span>
+          {question}  
+        </p>
+        {index === selectedFaq ? (
+          <div className={styles.arrowIcon} onClick={handleClick}>
+            <i className="fa-solid fa-chevron-up"></i>
+          </div>
+        ) : (
+          <div className={styles.arrowIcon} onClick={handleClick}>
+            <i className="fa-solid fa-chevron-down"></i>
+          </div>
+        )}
+      </div>
+      {index === selectedFaq && <p className={styles.faqAnsPara}>{answer}</p>}
+    </div>
+  );
+}
+
+export default Faq;
